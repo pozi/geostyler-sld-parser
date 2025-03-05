939f5c196e28cbc656b16b165155611f29b35f18
Terry McDonell <35645950+terry-longmacch@users.noreply.github.com>
2025-03-05 17:43:06 +1100

    fix: add dist files with missing wellknownnames

--- dist/SldStyleParser.js
+++ dist/SldStyleParser.js
@@ -928,6 +928,7 @@ export class SldStyleParser {
             case 'arrow':
             case 'arrowhead':
             case 'asterisk_fill':
+            case 'backslash':
             case 'circle':
             case 'cross':
             case 'cross2':
@@ -955,6 +956,7 @@ export class SldStyleParser {
             case 'rounded_square':
             case 'semi_circle':
             case 'shield':
+            case 'slash':
             case 'square':
             case 'square_with_corners':
             case 'star':
@@ -973,6 +975,13 @@ export class SldStyleParser {
             case 'shape://times':
             case 'shape://oarrow':
             case 'shape://carrow':
+            case 'brush://dense1':
+            case 'brush://dense2':
+            case 'brush://dense3':
+            case 'brush://dense4':
+            case 'brush://dense5':
+            case 'brush://dense6':
+            case 'brush://dense7':
                 markSymbolizer.wellKnownName = wellKnownName;
                 break;
             default:
